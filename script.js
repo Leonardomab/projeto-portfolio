427c512ed67167b12dc1e99ea9907d837015b200
mabiga <leonardo.flvt@gmail.com>
2023-07-05 10:42:59 -0300

    Mudança da imagem

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ function toggleMode() {
   html.classList.toggle("light")
 
   // pegar a tag img
-  const img = document.querySelector("profile image")
-
+  const img = document.querySelector("#profile img")
+  
+console.log(img);
   // subistituir a igamem
   if (html.classList.contains("light")) {
     // se iver light mode adicionar imagem light
-    img.seletAtribute("scr", "avatar.png")
+    img.setAttribute("src", "avatar.png");
   } else {
     // se tiver sem light mode deixar imagem normal
-    img.setAttribute("scr", "avatar-light.png")
+    img.setAttribute("src", "avatar-light.png");
   }
 }
